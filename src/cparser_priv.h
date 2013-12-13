896a611662887226c31e995a6065506fc9f1ca54
Mårten Wikström <marten.wikstrom@keystream.se>
2013-12-13 15:21:17 +0100

    Fix compilation error

--- src/cparser_priv.h
+++ src/cparser_priv.h
@@ -43,7 +43,7 @@
 struct cparser_node_ {
     cparser_node_type_t   type;      /**< Token type */ 
     uint32_t              flags;     /**< Flags */
-    const void                  *param;    /**< Token-dependent parameter */
+    void                  *param;    /**< Token-dependent parameter */
     const char                  *desc;     /**< A per-node description string */
     /** Pointer to the next sibling in the same level of the tree */
     cparser_node_t        *sibling;
